508400e7c7705f438bfccd898c3bcf13c7943138
dvirtz <dvirtz@gmail.com>
2020-03-16 22:33:19 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -22,35 +22,47 @@ struct B
 
 
 template <class C>
-class Check
+class CheckBase
 {
-
-private:
+protected:
     C& m_c;
+
+public:
+    CheckBase(C& _c) :m_c(_c) {}
+};
+
+template<class C>
+class Check;
+
+template<>
+class Check<A> : public CheckBase<A>
+{
+public:
+    using CheckBase::CheckBase;
+
+    bool is_on(int idx)
+    {
+        return m_c.configuration[idx].on;
+    }
+};
+
+template<>
+class Check<B> : public CheckBase<B>
+{
+public:
+    using CheckBase::CheckBase;
 
     int m_secondIndex = 1; /// i want this member *only* if the class is B
 
-public:
-    Check(C& _c) :m_c(_c) {}
-    
-    bool is_on(int idx);
+    bool is_on(int idx)
+    {
+        return m_c.configuration[m_secondIndex][idx].on;
+    }
 };
 
-// i want this for the case the class is A
-template<class T>
-typename std::enable_if<!std::is_array_v<decltype(std::declval<T>().configuration[0])>>
-bool Check<T>::is_on(int idx)
-{
-    return m_c.configuration[idx].on;
-}
-
-// i want this for the case the class is B (using the member m_secondIndex)
-template<class T>
-typename std::enable_if<std::is_array_v<decltype(std::declval<T>().configuration[0])>>
-bool Check<T>::is_on(int idx)
-{
-    return m_c.configuration[m_secondIndex][idx].on;
-}
+// deduction guides
+template<class C>
+Check(C& c) -> Check<C>;
  
 int main()
 {
